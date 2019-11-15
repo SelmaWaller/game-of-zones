1cf21924c0e4952b157aae04ed7e99d504f55cce
Selma Waller <selmawaller@gmail.com>
2019-11-15 19:41:28 +0100

    character select

--- js/characters.js
+++ js/characters.js
@@ -13,65 +13,88 @@ let getCharacters = async () => {
     return Promise.all(['Robert Arryn', 'Tommen Baratheon', 'Sandor Clegane', 'Theon Greyjoy', 'Tyrion Lannister', 'Oberyn Nymeros Martell', 'Lyra Mormont', 'Arya Stark', 'Daenerys Targaryen', 'Margaery Tyrell'].map(gotApi)); //wait for all promises: [gotApi('Robert Arryn'), gotApi('Robert I Baratheon') etc..]
 }
 
-let characters = [];
+window.characters = []
+//make an array for each character
+const images = ['images/arryn-badge.png', 'images/baratheon-badge.png', 'images/clegane-badge.png', 'images/greyjoy-badge.png', 'images/lannister-badge.png', 'images/martell-badge.png', 'images/mormont-badge.png', 'images/stark-badge.png', 'images/targaryen-badge.png', 'images/tyrell-badge.png'];
+//place badges in each card
+
+function createCharacter(character, index) {
+    let container = document.getElementById('characters');
+    badge = document.createElement('div');
+    badge.setAttribute('class', 'badge');
+    let idBtn = document.createElement('button');
+    idBtn.setAttribute('id', character.playedBy);
+    let name = document.createElement('h3');
+    name.textContent = (character.name);
+    let img = document.createElement('img');
+    img.setAttribute('src', images[index]);
+    img.setAttribute('id', 'a' + character.playedBy);
+    img.setAttribute('alt', character.name + ' badge');
+    let choose = document.createElement('button');
+    choose.setAttribute('class', 'choose');
+    choose.textContent = ('Choose');
+    let about = document.createElement('div');
+    about.setAttribute('class', 'aboutChar');
+    let gender = document.createElement('p');
+    gender.textContent = ('Gender: ' + character.gender);
+    let born = document.createElement('p');
+    born.textContent = ('Born: ' + character.born);
+    died = document.createElement('p');
+    died.textContent = ('Died: ' + character.died);
+    if (character.died === "") {
+        died.textContent = ('Died: Still alive/unknown');
+    }
+    let title = document.createElement('p');
+    title.textContent = ('Main Title: ' + character.titles.slice(0, 1));
+    if (character.titles == "") {
+        title.textContent = ('Main Title: No title');
+    }
+    let culture = document.createElement('p');
+    culture.textContent = ('Culture: ' + character.culture);
+    if (character.culture == "") {
+        culture.textContent = ('Culture: Unknown')
+    }
+    container.appendChild(badge);
+    badge.appendChild(idBtn);
+    idBtn.appendChild(name);
+    idBtn.appendChild(img);
+    idBtn.appendChild(about);
+    //idBtn.appendChild(choose);
+    about.appendChild(gender);
+    about.appendChild(born);
+    about.appendChild(died);
+    about.appendChild(title);
+    about.appendChild(culture);
+
+    /*select chars on click
+    idBtn.onclick = () => {
+        let player1 = document.getElementById('player1');
+        let player2 = document.getElementById('player2');
+
+        player1.value = character.name;
+        player2.value = character.name;
+        localStorage.setItem('player1', character.name);
+        localStorage.setItem('player2', character.name);
+        const chosenPlayer1 = localStorage.getItem('player1');
+        const chosenPlayer2 = localStorage.getItem('player2');
+        console.log('Player 1: ' + chosenPlayer1);
+        console.log('Player 2: ' + chosenPlayer2);
+    }*/
+
+    document.getElementById('startGame').addEventListener('click', function () {
+        let player1 = document.getElementById('player1');
+        let player2 = document.getElementById('player2');
+        localStorage.setItem('player1', player1.value);
+        localStorage.setItem('player2', player2.value);
+        let chosenCharacter1 = localStorage.getItem('player1', player1.value);
+        let chosenCharacter2 = localStorage.getItem('player2', player2.value);
+        console.log('Player 1: ' + chosenCharacter1);
+        console.log('Player 2: ' + chosenCharacter2);
+    });
+}
 
 getCharacters().then(chars => {
-    characters = chars;
+    window.characters = chars;
     console.log(chars);
-    for (i = 0; i < chars.length; i++) {
-        images = ['images/arryn-badge.png', 'images/baratheon-badge.png', 'images/clegane-badge.png', 'images/greyjoy-badge.png', 'images/lannister-badge.png', 'images/martell-badge.png', 'images/mormont-badge.png', 'images/stark-badge.png', 'images/targaryen-badge.png', 'images/tyrell-badge.png'];
-        let container = document.getElementById('characters');
-        badge = document.createElement('div');
-        badge.setAttribute('class', 'badge');
-        let idBtn = document.createElement('button');
-        idBtn.setAttribute('id', chars[i].playedBy);
-        let name = document.createElement('h3');
-        name.textContent = (chars[i].name);
-        let img = document.createElement('img');
-        img.setAttribute('src', images[i]);
-        img.setAttribute('id', 'a' + chars[i].playedBy);
-        img.setAttribute('alt', chars[i].name + ' badge');
-        let about = document.createElement('div');
-        about.setAttribute('class', 'aboutChar');
-        let gender = document.createElement('p');
-        gender.textContent = ('Gender: ' + chars[i].gender);
-        let born = document.createElement('p');
-        born.textContent = ('Born: ' + chars[i].born);
-        died = document.createElement('p');
-        died.textContent = ('Died: ' + chars[i].died);
-        if (chars[i].died === "") {
-            died.textContent = ('Died: Still alive/unknown');
-        }
-        let title = document.createElement('p');
-        title.textContent = ('Main Title: ' + chars[i].titles.slice(0, 1));
-        if (chars[i].titles == "") {
-            title.textContent = ('Main Title: No title');
-        }
-        let culture = document.createElement('p');
-        culture.textContent = ('Culture: ' + chars[i].culture);
-        if (chars[i].culture == "") {
-            culture.textContent = ('Culture: Unknown')
-        }
-        container.appendChild(badge);
-        badge.appendChild(idBtn);
-        idBtn.appendChild(name);
-        idBtn.appendChild(img);
-        idBtn.appendChild(about);
-        about.appendChild(gender);
-        about.appendChild(born);
-        about.appendChild(died);
-        about.appendChild(title);
-        about.appendChild(culture);
-
-        idBtn.onclick = function () {
-            let player1 = document.getElementById('player1');
-            let player2 = document.getElementById('player2');
-            for (i = 0; i < chars.length; i++) {
-                player1.value = chars[i].name;
-                player2.value = chars[i].name;
-                localStorage.setItem('Player 1: ', player1.value);
-                localStorage.setItem('Player 2: ', player2.value);
-            }
-        }
-    }
+    chars.forEach((char, index) => createCharacter(char, index))
 });
